0c4817048a0db4192d509e65b92f24b0f640f208
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-09-13 16:44:26 +0530

    Create new-data-element.py

---
+++ new-data-element.py
@@ -0,0 +1,12 @@
+adapters.addSingleVal("regex", "pattern");
+    adapters.addBool("creditcard").addBool("date").addBool("digits").addBool("email").addBool("number").addBool("url");
+    adapters.addMinMax("length", "minlength", "maxlength", "rangelength").addMinMax("range", "min", "max", "range");
+    adapters.addMinMax("minlength", "minlength").addMinMax("maxlength", "minlength", "maxlength");
+    adapters.add("equalto", ["other"], function (options) {
+      
+      number: { number: true },
+		digits: { digits: true },
+		creditcard: { creditcard: true }
+	},
+
+	addClassRules: function( className, rules ) {
